a76dc01289554a503b9dbaa3e1441ecb96a03556
Hugh Wimberly <hwimberly@ginger.io>
2020-11-25 11:57:30 -0800

    Echo commands to fish

--- install.sh
+++ install.sh
@@ -7,20 +7,19 @@ sudo apt-get -y install fish
 
 # Make fish the default shell
 echo $(which fish) | sudo tee -a /etc/shells
-chsh -s $(which fish)
+sudo chsh -s $(which fish)
 
 # Add the oh-my-fish extension
 curl -L https://get.oh-my.fish | fish
 
 # Install the bobthefish theme
-omf install bobthefish
+echo "omf install bobthefish" | fish
 
 # Use nerdfonts theme
-set -U theme_nerd_fonts yes
+echo "set -U theme_nerd_fonts yes" | fish
 
 # Disable the fallback virtualenv prompt
-set -U VIRTUAL_ENV_DISABLE_PROMPT 1
-
+echo "set -U VIRTUAL_ENV_DISABLE_PROMPT 1" | fish
 
 # Install personal cli utilities
 sudo apt-get -y install tldr exa
