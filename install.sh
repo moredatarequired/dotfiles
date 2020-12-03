e297e01f4781162ee64030db3fe5b7a6bb66ae40
Hugh Wimberly <hwimberly@ginger.io>
2020-12-03 02:15:11 +0000

    Move fish script out of install.sh

--- install.sh
+++ install.sh
@@ -8,24 +8,15 @@ sudo apt-get -y install fish
 # Copy my okta config to my home directory
 cp ~/dotfiles/.okta_aws_login_config ~/
 
-# Make fish the default shell
-echo $(which fish) | sudo tee -a /etc/shells
-sudo chsh -s $(which fish)
-
-# Add the oh-my-fish extension
-curl -L https://get.oh-my.fish | fish
-
-# Install the bobthefish theme
-echo "omf install bobthefish" | fish
-
-# Use nerdfonts theme
-echo "set -U theme_nerd_fonts yes" | fish
-
-# Disable the fallback virtualenv prompt
-echo "set -U VIRTUAL_ENV_DISABLE_PROMPT 1" | fish
-
 # Install personal cli utilities
 sudo apt-get -y install tldr exa
 
 # Run tldr for the first time
 tldr - > /dev/null
+
+# Make fish the default shell
+echo $(which fish) | sudo tee -a /etc/shells
+sudo chsh -s $(which fish)
+
+# Run fish configuration
+fish ~/dotfiles/setup.fish
